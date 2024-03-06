f4336e4a05d2bef2ee05d57af6dd3cde5b94aa3e
Hannah <hg772921@ohio.edu>
2024-03-05 23:01:13 -0500

    Fixed the main menu. It works!!

--- XclusionZone-2023/Assets/Scripts/Main Menu.cs
+++ XclusionZone-2023/Assets/Scripts/Main Menu.cs
@@ -1,19 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class MainMenu : MonoBehaviour
+public class MainMenu : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-  public void OnStartButton()
-  {
-      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-  }
+    public Image centerNavigationImage;
+    public Sprite playSprite;
+    public Sprite quitSprite;
 
-  public void OnQuitButton()
-  {
-      Debug.Log ("Quit");
-      Application.Quit();
-  }
+    private Sprite originalSprite;
+
+    void Start()
+    {
+        // Store the original sprite
+        originalSprite = centerNavigationImage.sprite;
+    }
+
+    public void OnStartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnQuitButton()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Change the image when pointer enters the button
+        if (gameObject.name == "StartButton")
+        {
+            centerNavigationImage.sprite = playSprite;
+        }
+        else if (gameObject.name == "QuitButton")
+        {
+            centerNavigationImage.sprite = quitSprite;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Reset the image when pointer exits the button
+        centerNavigationImage.sprite = originalSprite;
+    }
 }
